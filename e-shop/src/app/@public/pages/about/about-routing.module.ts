1fb80ba972249661fd8128c361ef925573dfc5cd
rom <romvargas81@gmail.com>
2020-11-08 11:26:40 -0600

    lazy loading not working

--- e-shop/src/app/@public/pages/about/about-routing.module.ts
+++ e-shop/src/app/@public/pages/about/about-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ContactComponent } from './contact.component'; 
+import { AboutComponent } from './about.component';
 
 const routes: Routes = [
   {
-    path: '', component: ContactComponent
+    path: '', component: AboutComponent
   }
 ];
 
@@ -12,4 +12,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class ContactRoutingModule { }
+export class AboutRoutingModule { }
